01581730bb4e530edc60a3e792b6f624ca5e3b79
Subhranil Maity <fightingdj1@gmail.com>
2025-06-22 18:16:51 +0530

    made the popup beautiful

--- popup.js
+++ popup.js
@@ -1,8 +1,20 @@
 document.getElementById("add").addEventListener("click", () => {
   const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value.trim();
-
-  if (!email || !password) return alert("Both fields required.");
+	const errorString = document.getElementById("error-string");
+	
+	const emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+  if (!email || !password) {
+		errorString.hidden = false;
+		errorString.textContent = "Both email and password are required.";
+		return;
+	}
+	if(!emailRegex.test(email)) {
+		errorString.hidden = false;
+		errorString.textContent = "Please enter a valid Gmail address.";
+		return;
+	}
+	
 
   chrome.storage.local.get(["locks"], (data) => {
     const locks = data.locks || {};
